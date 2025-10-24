67249d2d95c388208b903c62e073875944a61458
Maithri Rao <maithrirao30@gmail.com>
2025-10-24 23:52:48 +0200

    Initial Commit 🎉

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Python shell flake";
+
+  inputs = {
+    # nixpkgs.url = "github:nixos/nixpkgs/a4e9f9a895197d62b3546021ee5d58a4596bda9c";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    lib = nixpkgs.lib;
+    forAllSystems = function: lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+    ] function;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+          "postman"
+        ];
+      };
+
+      pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+        jupyterlab
+        langchain
+        langchain-community
+        langchain-openai
+        sentence-transformers
+        seaborn
+        nltk
+        boto3
+        dvc
+        python-dotenv
+        pypdf
+        dvc-s3
+        flask-cors
+      ]);
+    in {
+      default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          pythonEnv
+          awscli
+          postman
+        ];
+      };
+    });
+  };
+}
