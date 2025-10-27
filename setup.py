1dc0458e9736d70f621850f25ea9aa67dc1289d5
Maithri Rao <maithrirao30@gmail.com>
2025-10-27 10:40:28 +0100

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_RAG_chatbot",
+    version="0.1.0",
+    author="Maithri Rao",
+    author_email="maithrirao30@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
