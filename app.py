55f8f1c6de8cc93be3e7d90bbd485cfb11c78bab
Maithri Rao <maithrirao30@gmail.com>
2025-10-27 10:15:49 +0100

    Implement core Flask RAG application and routes

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import ChatOpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+
+app = Flask(__name__)
+
+
+load_dotenv()
+
+embeddings = download_hugging_face_embeddings()
+print("DEBUG: Embeddings loaded successfully.")
+
+index_name = "medical-chatbot"
+
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+try:
+    docsearch = PineconeVectorStore.from_existing_index(
+        index_name=index_name,
+        embedding=embeddings
+    )
+    print("DEBUG: Pinecone connection successful.") # CHECK 2
+except Exception as e:
+    print(f"CRITICAL ERROR: Failed to connect to Pinecone index: {e}")
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+chatModel = ChatOpenAI(model="gpt-4o")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
